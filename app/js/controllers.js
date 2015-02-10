2204d1fc542eeb2b6659194c854c11fd2574452d
dwightgnjohnson <dwightgnjohnson@gmail.com>
2015-02-10 13:56:23 -0800

    end of step 5

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,10 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
 
-  $scope.orderProp = 'age';
-});
+    $scope.orderProp = 'age';
+  }]);
