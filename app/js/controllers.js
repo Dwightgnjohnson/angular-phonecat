a7f717937d8584a3bc122f025fddb207ed783f29
dwightgnjohnson <dwightgnjohnson@gmail.com>
2015-02-10 15:14:36 -0800

    end of step 7

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,10 +1,19 @@
-var phonecatApp = angular.module('phonecatApp', []);
+'use strict';
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
+/* Controllers */
+
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function($scope, $http) {
     $http.get('phones/phones.json').success(function(data) {
       $scope.phones = data;
     });
 
     $scope.orderProp = 'age';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
   }]);
